6f7984913f00e3fc75b19e470333eace2538a090
Lounnaci-gi <ahmedlounnaci@gmail.com>
2025-12-02 16:31:51 +0100

    02112025+7

--- frontend/src/components/DevisForm.js
+++ frontend/src/components/DevisForm.js
@@ -1581,13 +1581,41 @@ const DevisForm = ({ user }) => {
                       // Sort families alphabetically
                       const sortedFamilies = Object.keys(groupedArticles).sort();
                       
+                      // Function to convert number to Roman numerals
+                      const toRoman = (num) => {
+                        const romanNumerals = [
+                          { value: 1000, numeral: 'M' },
+                          { value: 900, numeral: 'CM' },
+                          { value: 500, numeral: 'D' },
+                          { value: 400, numeral: 'CD' },
+                          { value: 100, numeral: 'C' },
+                          { value: 90, numeral: 'XC' },
+                          { value: 50, numeral: 'L' },
+                          { value: 40, numeral: 'XL' },
+                          { value: 10, numeral: 'X' },
+                          { value: 9, numeral: 'IX' },
+                          { value: 5, numeral: 'V' },
+                          { value: 4, numeral: 'IV' },
+                          { value: 1, numeral: 'I' }
+                        ];
+                        
+                        let result = '';
+                        for (const { value, numeral } of romanNumerals) {
+                          while (num >= value) {
+                            result += numeral;
+                            num -= value;
+                          }
+                        }
+                        return result;
+                      };
+                      
                       // Render articles grouped by family with family headers as separators
                       return sortedFamilies.map((family, familyIndex) => (
                         <React.Fragment key={family}>
                           {/* Family Header as separator */}
                           <tr className="bg-gray-100 dark:bg-gray-700">
                             <td colSpan="6" className="px-4 py-2 text-sm font-bold text-gray-700 dark:text-gray-300">
-                              {family}
+                              {toRoman(familyIndex + 1)}. {family}
                             </td>
                           </tr>
                           
