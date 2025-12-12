6f9c11beb34ff31f1588112ec734462ccb90d4f2
Lounnaci <ahmedlounnaci@gmail.com>
2025-12-12 12:14:42 +0100

    12122025+

--- frontend/src/components/DemandeList.js
+++ frontend/src/components/DemandeList.js
@@ -228,8 +228,7 @@ const DemandeList = ({ user }) => {
             .branchement-diameter {
               position: absolute;
               top: 18.5cm;
-              left: 5cm;
-              text-transform: lowercase;
+              left: 4cm;
             }
             .agency-commune {
               position: absolute;
@@ -239,7 +238,28 @@ const DemandeList = ({ user }) => {
             .creation-date {
               position: absolute;
               top: 22cm;
+              left: 6.5cm;
+            }
+            .owner-underline {
+              position: absolute;
+              top: 9.5cm;
               left: 5cm;
+              width: 100px; /* Ajustez cette valeur selon la longueur souhaitée */
+              border-bottom: 1px solid black;
+            }
+            .tenant-underline {
+              position: absolute;
+              top: 9.5cm;
+              left: 7cm;
+              width: 80px; /* Ajustez cette valeur selon la longueur souhaitée */
+              border-bottom: 1px solid black;
+            }
+            .mandataire-underline {
+              position: absolute;
+              top: 9.5cm;
+              left: 8.5cm;
+              width: 100px; /* Ajustez cette valeur selon la longueur souhaitée */
+              border-bottom: 1px solid black;
             }
           </style>
         </head>
@@ -256,6 +276,9 @@ const DemandeList = ({ user }) => {
           <div class="branchement-diameter">${clientData?.DiametreBranchement || ''}</div>
           <div class="agency-commune">${agence?.Commune || ''}</div>
           <div class="creation-date">${demandeData?.DateCreation ? new Date(demandeData.DateCreation).toLocaleDateString('fr-FR') : ''}</div>
+          ${clientData?.StatutOccupation === 'PROPRIETAIRE' ? '<div class="owner-underline"></div>' : ''}
+          ${clientData?.StatutOccupation === 'LOCATAIRE' ? '<div class="tenant-underline"></div>' : ''}
+          ${clientData?.StatutOccupation === 'MANDATAIRE' ? '<div class="mandataire-underline"></div>' : ''}
         </body>
         </html>
       `);
